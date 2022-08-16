7e32cd7db3946cc0b2254ea2538b25f298eb27bb
chenjie <chenjie@szy.cn>
2022-08-16 14:27:24 +0800

    feat: stylelint add rules

--- packages/stylelint-config/rules/standard.js
+++ packages/stylelint-config/rules/standard.js
@@ -1,3 +1,17 @@
 module.exports = {
-  extends: ['stylelint-config-standard-scss']
+  extends: ['stylelint-config-standard'],
+  overrides: [
+    {
+      files: ['**/*.vue'],
+      customSyntax: 'postcss-html'
+    },
+    {
+      files: ['**/*.less'],
+      customSyntax: 'postcss-less'
+    },
+    {
+      files: ['**/*.scss'],
+      customSyntax: 'postcss-scss'
+    }
+  ]
 }
